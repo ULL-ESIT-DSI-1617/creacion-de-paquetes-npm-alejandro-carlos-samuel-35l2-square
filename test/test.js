9efe0b453d1e569bcfc12ce1c51ccffeeebcb9a2
Carlos de Armas <alu0100816167@ull.edu.es>
2017-04-25 13:27:25 +0100

    añadiendo pruebas y gulpfile

---
+++ test/test.js
@@ -0,0 +1,44 @@
+let Shape = require('@alejandro-carlos-samuel/ull-shape');
+let Square = require('../lib/square.js');
+let assert = require('chai').assert;
+let expect = require('chai').expect;
+
+describe("Shape", function() {
+    it("Se crea correctamente el objeto Shape", function() {
+        let figura = new Shape({
+            width: 8
+        }, 'Square');
+        assert.typeOf(figura, 'Object');
+        expect(figura).to.not.be.undefined;
+    })
+});
+
+describe("Square", function() {
+    it("Se crea correctamente el objeto Square", function() {
+        let figura = new Square({
+            width: 3
+        });
+        assert.typeOf(figura, 'Object');
+        expect(figura).to.not.be.undefined;
+
+    })
+});
+
+describe("getArea()", function() {
+    it("Se calcula correctamente el área usando Shape", function() {
+        let figura = new Shape({
+            width: 8
+        }, 'Square');
+        let squareArea = figura.getArea();
+        expect(squareArea).to.equal(64);
+    })
+    
+    it("Se calcula correctamente el área usando Square", function() {
+        let figura = new Square({
+            width: 3
+        });
+        let squareArea = figura.getArea();
+        expect(squareArea).to.equal(9);
+    })
+    
+});
